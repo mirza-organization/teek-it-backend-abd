6cfd71f81bf71f68962550fe541895d19c968c91
Hassan Hafeez <hhassanhafeez7@gmail.com>
2020-11-20 10:20:32 +0500

    updated shopkeeper dashboard table and is_viewed logic

--- app/Orders.php
+++ app/Orders.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Orders extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function order_items()
+    {
+        return $this->hasMany(OrderItems::class, 'order_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasManyThrough
+     */
+    public function order_item_products()
+    {
+        return $this->hasManyThrough(OrderItems::class,Products::class);
+    }
 }
