e78da0c461558f92cb769649b56307c357a500c9
Hassan Hafeez <hhassanhafeez7@gmail.com>
2020-12-22 19:31:08 +0500

    update bank names

--- app/Mail/StoreRegisterMail.php
+++ app/Mail/StoreRegisterMail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class StoreRegisterMail extends Mailable implements ShouldQueue
+class StoreRegisterMail extends Mailable
 {
     use Queueable, SerializesModels;
 
