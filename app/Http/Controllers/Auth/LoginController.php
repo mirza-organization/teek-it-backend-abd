0e019cd56d5f504b37124463bc46d91eb73e1830
ahmad11 <108904733+mirzaabdullahizhar@users.noreply.github.com>
2022-10-03 14:30:27 +0500

    flash message shown on login error

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -39,7 +39,9 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function login(\Illuminate\Http\Request $request) {
+    public function login(\Illuminate\Http\Request $request)
+    {
+
         $this->validateLogin($request);
         // If the class is using the ThrottlesLogins trait, we can automatically throttle
         // the login attempts for this application. We'll key this by the username and
@@ -52,13 +54,14 @@ class LoginController extends Controller
         // This section is the only change
         if ($this->guard()->validate($this->credentials($request))) {
             $user = $this->guard()->getLastAttempted();
-            
+            // dd($user);
+
             // Make sure the user is active
             if ($user->is_active && $this->attemptLogin($request) && $user->email_verified_at != null) {
                 // Send the normal successful login response
-                if ($user->hasRole('seller') || $user->hasRole('superadmin')){
+                if ($user->hasRole('seller') || $user->hasRole('superadmin')) {
                     return $this->sendLoginResponse($request);
-                }else{
+                } else {
                     return redirect()
                         ->route('login')
                         ->withInput($request->only($this->username(), 'remember'))
@@ -70,7 +73,7 @@ class LoginController extends Controller
                 $this->incrementLoginAttempts($request);
                 if ($user->email_verified_at == null) {
                     Flash::message('Email not verified, verify your email first.');
-                }else{
+                } else {
                     Flash::message('You are not Activated, kindly contact admin.');
                 }
 
@@ -85,12 +88,12 @@ class LoginController extends Controller
         // to login and redirect the user back to the login form. Of course, when this
         // user surpasses their maximum number of attempts they will get locked out.
         $this->incrementLoginAttempts($request);
-
-        return $this->sendFailedLoginResponse($request);
+        //return $this->sendFailedLoginResponse($request);
+        return $this->invalidCreds($request);
     }
-//    public function sendFailedLoginResponse(Request $request)
-//    {
-//        print_r($request->all());die;
-//        return redirect()->route('login'); //just an available path to test
-//    }
+    //    public function sendFailedLoginResponse(Request $request)
+    //    {
+    //        print_r($request->all());die;
+    //        return redirect()->route('login'); //just an available path to test
+    //    }
 }
