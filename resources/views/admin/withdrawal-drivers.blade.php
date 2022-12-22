3795be395a5ab51ed29c5ae9439237a4c50d2f6d
ahmad11 <108904733+mirzaabdullahizhar@users.noreply.github.com>
2022-12-22 16:20:32 +0500

    Modifications in withdrawal-drivers blade file

--- resources/views/admin/withdrawal-drivers.blade.php
+++ resources/views/admin/withdrawal-drivers.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin.app')
 @section('content')
     <div class="jumbotron">
-        <h1 class="display-4">Dear User</h1>
+        <h1 class="display-4">Dear Admin</h1>
         <p class="lead">Currently our drivers table has no withdrawl request column.</p>
         <hr class="my-4">
         <p>It will be up & runnung as soon as we resolve it.</p>
