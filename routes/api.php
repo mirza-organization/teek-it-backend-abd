ec131d7afba6676150837e7c1c1ce0f4d2c24a38
Mirza Abdullah Izhar <aagiftforall@gmail.com>
2023-03-10 13:54:08 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -194,10 +194,29 @@ Route::get('payment_intent', function () {
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, "amount=$amount&currency=$currency&metadata[integration_check]=accept_a_payment");
     curl_setopt($ch, CURLOPT_USERPWD, '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' . ':' . '');
+    
+    $headers = array();
+    $headers[] = 'Content-Type: application/x-www-form-urlencoded';
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-    // Test Account Key
-    // curl_setopt($ch, CURLOPT_USERPWD, 'sk_test_51IY9sYIiDDGv1gaVKsxU0EXr96lHcCvwXHwYAdN81Cqrj1TBL4HErJpczWJpYFIQ1qbCOQxnxIM3UfsBtWC2MKeD00QRkUKg6q' . ':' . '');
+    $result = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+    }
+    curl_close($ch);
+    return response()->json(json_decode($result), 200);
+});
 
+Route::get('payment_intent/test', function () {
+    $ch = curl_init();
+    $amount = $_REQUEST['amount'];
+    $currency = $_REQUEST['currency'];
+    curl_setopt($ch, CURLOPT_URL, 'https://api.stripe.com/v1/payment_intents');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "amount=$amount&currency=$currency&metadata[integration_check]=accept_a_payment");
+    curl_setopt($ch, CURLOPT_USERPWD, 'sk_test_51IY9sYIiDDGv1gaVKsxU0EXr96lHcCvwXHwYAdN81Cqrj1TBL4HErJpczWJpYFIQ1qbCOQxnxIM3UfsBtWC2MKeD00QRkUKg6q' . ':' . '');
+    
     $headers = array();
     $headers[] = 'Content-Type: application/x-www-form-urlencoded';
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
@@ -239,6 +258,6 @@ Route::fallback(function () {
     return response()->json([
         'data' => [],
         'status' => false,
-        'message' => 'Page Not Found.'
+        'message' => 'API Not Found.'
     ], 404);
 });
