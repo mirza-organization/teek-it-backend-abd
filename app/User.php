b3e30b9425f22e559d0a2579e62d3271572f015e
Muhammad Abdullah Mirza <61614849+mirza-mam@users.noreply.github.com>
2023-04-24 17:49:23 +0500

    Optimized code to Bootstrap5 (#22)
    * Categories controller for API has been Skinned along with FAT Categories Model
    
    * minor change
    
    * Categories Controller and Model API Updated According to Solid Principles
    
    * Update Categories.php
    
    * git pull
    
    * Check Viewproducts by category ID
    
    * Pagination issue resolved
    
    * Categories Controller Optimized
    
    * changes in UserController Sellers function and Users Model, I have created a new method name getParentwithChildSellers() in UserModel
    
    * add space
    
    * Mazeed Optimization
    
    * Categories Controller responses changed from $this->response to JsonResponseCustom::
    
    * I have changed product controllers api responses
    
    * 3 methods of Products Controller has been changed to Eloquent
    
    * Products Controller Part 2
    
    * Products Controller Api Finished
    
    * Products controller to Optimized Code (#21)
    
    * Promocode update
    
    * Promocodes Code Optimized
    
    * Major Changes
    
    * Bootstrap5 v (#12)
    
    * Bootstrap 5 Updated Second Time
    
    * Correct Bootstrap Models | Edit Button(Admin Side)  in Promocodes needs dynamic data for functioning
    
    * New service has been created for JsonResponse
    
    * Response for Catgories Api Class has been set through JsonResponseCustom sericvice
    
    * Update JsonResponseCustom.php
    
    * Optimized code bs5v child (#13)
    
    * New JsonResponseCustom File
    
    * Change in Jason response
    
    ---------
    
    Co-authored-by: Sabah Uddin <mirzaabdullahizhar@gmail.com>
    
    ---------
    
    Co-authored-by: Sabah Uddin <mirzaabdullahizhar@gmail.com>
    
    * Routes Updated + Livewire Added
    
    * Routes Updated + Livewire Added 2
    
    * Minor Modifications
    
    * Livewire Added
    
    * Bootstrap5 v into Version 8 (#14)
    
    * Bootstrap 5 Updated Second Time
    
    * Correct Bootstrap Models | Edit Button(Admin Side)  in Promocodes needs dynamic data for functioning
    
    * New service has been created for JsonResponse
    
    * Response for Catgories Api Class has been set through JsonResponseCustom sericvice
    
    * Update JsonResponseCustom.php
    
    * Optimized code bs5v child (#13)
    
    * New JsonResponseCustom File
    
    * Change in Jason response
    
    ---------
    
    Co-authored-by: Sabah Uddin <mirzaabdullahizhar@gmail.com>
    
    ---------
    
    Co-authored-by: Sabah Uddin <mirzaabdullahizhar@gmail.com>
    
    * Routes Updated
    
    * Update api.php
    
    * Update header-links.blade.php
    
    * Update header-links.blade.php
    
    * Bs5 Folder Deleteed
    
    * BS 5 Included
    
    * Optimized code bs5 child into Bootstrap 5 (#15) (#16)
    
    * Categories controller for API has been Skinned along with FAT Categories Model
    
    * minor change
    
    * Categories Controller and Model API Updated According to Solid Principles
    
    * Update Categories.php
    
    * git pull
    
    * Check Viewproducts by category ID
    
    * Pagination issue resolved
    
    * Categories Controller Optimized
    
    * changes in UserController Sellers function and Users Model, I have created a new method name getParentwithChildSellers() in UserModel
    
    * add space
    
    * Mazeed Optimization
    
    * Categories Controller responses changed from $this->response to JsonResponseCustom::
    
    * I have changed product controllers api responses
    
    * 3 methods of Products Controller has been changed to Eloquent
    
    * Products Controller Part 2
    
    ---------
    
    Co-authored-by: Sabah Uddin <mirzaabdullahizhar@gmail.com>
    
    * First change in new productcontroller
    
    ---------
    
    Co-authored-by: Sabah Uddin <mirzaabdullahizhar@gmail.com>
    
    ---------
    
    Co-authored-by: Sabah Uddin <mirzaabdullahizhar@gmail.com>

--- app/User.php
+++ app/User.php
@@ -172,7 +172,7 @@ class User extends Authenticatable implements JWTSubject
             Thank you for registering on ' . env('APP_NAME') . '.
             <br>
             Your store has been approved. Please login to the
-            <a href="' . env('FRONTEND_URL') . '">Store</a> to update your store
+            <a href="' . url('/') . '">Store</a> to update your store
             <br><br><br>
                      </html>';
         $subject = env('APP_NAME') . ': Account Approved!';
@@ -188,5 +188,6 @@ class User extends Authenticatable implements JWTSubject
             static::sendStoreApprovedEmail($user);
         }
     }
+
 }
 
